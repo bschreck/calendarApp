a472c12067a995a13bd944002a1230430b0dd23f
Ben Schreck <bschreck@mit.edu>
2013-10-14 13:02:58 -0400

    switched to modular style (maybe slower?)

--- app.rb
+++ app.rb
@@ -1,52 +1,54 @@
 require 'sinatra'
+require 'sinatra/base'
 require 'stripe'
 
-set :publishable_key, ENV['PUBLISHABLE_KEY']
-set :secret_key, ENV['SECRET_KEY']
+class MyApp < Sinatra::Base
+  set :publishable_key, ENV['PUBLISHABLE_KEY']
+  set :secret_key, ENV['SECRET_KEY']
 
-Stripe.api_key = settings.secret_key
+  Stripe.api_key = settings.secret_key
 
-get '/' do
-  erb :index
+  get '/' do
+    erb :index
+  end
+
+  get '/order' do
+    erb :order
+  end
+
+  get '/info' do
+    erb :info
+  end
+
+  get '/creators' do
+    erb :creators
+  end
+
+  get '/mailinglist' do
+    erb :mailinglist
+  end
+
+  post '/charge' do
+    # Amount in cents
+    @amount = 500
+
+    customer = Stripe::Customer.create(
+      :email => 'customer@example.com',
+      :card  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :amount      => @amount,
+      :description => 'Sinatra Charge',
+      :currency    => 'usd',
+      :customer    => customer.id
+    )
+
+    erb :charge
+  end
+
+  error Stripe::CardError do
+    env['sinatra.error'].message
+  end
+
 end
-
-get '/order' do
-  erb :order
-end
-
-get '/info' do
-  erb :info
-end
-
-get '/creators' do
-  erb :creators
-end
-
-get '/mailinglist' do
-  erb :mailinglist
-end
-
-post '/charge' do
-  # Amount in cents
-  @amount = 500
-
-  customer = Stripe::Customer.create(
-    :email => 'customer@example.com',
-    :card  => params[:stripeToken]
-  )
-
-  charge = Stripe::Charge.create(
-    :amount      => @amount,
-    :description => 'Sinatra Charge',
-    :currency    => 'usd',
-    :customer    => customer.id
-  )
-
-  erb :charge
-end
-
-error Stripe::CardError do
-  env['sinatra.error'].message
-end
-
-__END__
--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
 require './app'
+
 use Rack::Static, :urls => ['/images', '/jquery', 'bootstrap'], :root => 'public'
-run Sinatra::Application
+run MyApp
