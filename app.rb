421df1db40179182308690d61d283dce57167bc4
Ben Schreck <bschreck@mit.edu>
2013-10-14 16:13:41 -0400

    force ssl

--- app.rb
+++ app.rb
@@ -3,6 +3,22 @@ require 'sinatra/base'
 require 'stripe'
 
 class MyApp < Sinatra::Base
+  configure :development, :test do
+    set :host, 'localhost:5000'
+    set :force_ssl, false
+  end
+  configure :production do
+    set :host, 'benschreckshits.com'
+    set :force_ssl, true
+  end
+
+  #before do
+    #content_type :json
+    #ssl_whitelist = ['/calendar.ics']
+    #if settings.force_ssl && !request.secure? && !ssl_whitelist.include?(request.path_info)
+      #halt json_status 400, "Please use SSL at https://#{settings.host}"
+    #end
+  #end
   set :publishable_key, ENV['PUBLISHABLE_KEY']
   set :secret_key, ENV['SECRET_KEY']
 
